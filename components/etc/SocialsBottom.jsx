b2b4431efe065901e9158675df7b909e2e163a86
Moreno <moreno-rizvic@outlook.de>
2024-08-26 02:50:46 +0200

    commit

--- components/etc/SocialsBottom.jsx
+++ components/etc/SocialsBottom.jsx
@@ -10,49 +10,61 @@ import {
 
 export const SocialsBottom = () => {
   return (
-    <div className="w-full flex items-center justify-center space-x-4 bg-background py-3">
-      <Link
-        href={"/"}
-        target="_blank"
-        className="hover:scale-125 transition duration-200"
-      >
-        <RiDiscordFill className="w-[36px] h-[36px]" />
-      </Link>
-      <Link
-        href={"https://www.reddit.com/user/Scalyx_Token/"}
-        target="_blank"
-        className="hover:scale-125 transition duration-200"
-      >
-        <RiRedditFill className="w-[36px] h-[36px]" />
-      </Link>
-      <Link
-        href={"https://x.com/ScalyxToken"}
-        target="_blank"
-        className="hover:scale-125 transition duration-200"
-      >
-        <FaSquareXTwitter className="w-[36px] h-[36px]" />
-      </Link>
-      <Link
-        href={"https://www.facebook.com/profile.php?id=61561780352205"}
-        target="_blank"
-        className="hover:scale-125 transition duration-200"
-      >
-        <RiFacebookCircleFill className="w-[36px] h-[36px]" />
-      </Link>
-      <Link
-        href={"https://www.instagram.com/ScalyxToken"}
-        target="_blank"
-        className="hover:scale-125 transition duration-200"
-      >
-        <RiInstagramFill className="w-[36px] h-[36px]" />
-      </Link>
-      <Link
-        href={"https://t.me/Scalyx1"}
-        target="_blank"
-        className="hover:scale-125 transition duration-200"
-      >
-        <FaTelegram className="w-[36px] h-[36px]" />
-      </Link>
+    <div className="w-full flex items-center justify-center flex-col bg-background py-3 mx-4 rounded-3xl">
+      <h1 className="font-black">SOCIALS</h1>
+      <div className="space-x-4 flex items-center justify-center">
+        <Link
+          href={"/"}
+          target="_blank"
+          className="hover:scale-125 transition duration-200"
+        >
+          <RiDiscordFill className="w-[36px] h-[36px]" />
+        </Link>
+        <Link
+          href={"https://www.reddit.com/user/Scalyx_Token/"}
+          target="_blank"
+          className="hover:scale-125 transition duration-200"
+        >
+          <RiRedditFill className="w-[36px] h-[36px]" />
+        </Link>
+        <Link
+          href={"https://x.com/ScalyxToken"}
+          target="_blank"
+          className="hover:scale-125 transition duration-200"
+        >
+          <FaSquareXTwitter className="w-[36px] h-[36px]" />
+        </Link>
+        <Link
+          href={"https://www.facebook.com/profile.php?id=61561780352205"}
+          target="_blank"
+          className="hover:scale-125 transition duration-200"
+        >
+          <RiFacebookCircleFill className="w-[36px] h-[36px]" />
+        </Link>
+        <Link
+          href={"https://www.instagram.com/ScalyxToken"}
+          target="_blank"
+          className="hover:scale-125 transition duration-200"
+        >
+          <RiInstagramFill className="w-[36px] h-[36px]" />
+        </Link>
+        <Link
+          href={"https://t.me/Scalyx1"}
+          target="_blank"
+          className="hover:scale-125 transition duration-200"
+        >
+          <FaTelegram className="w-[36px] h-[36px]" />
+        </Link>
+      </div>
+      <h2 className="mt-1 text-sm">
+        Or contact us by mail:{" "}
+        <Link
+          href={"mailto:contact@scalyx.io"}
+          className="text-blue-500 hover:underline underline-offset-2"
+        >
+          contact@scalyx.io
+        </Link>
+      </h2>
     </div>
   );
 };
--- components/sections/DownSection.jsx
+++ components/sections/DownSection.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { TokenomicsPieChart } from "../etc/TokenomicsPieChart";
 import { SocialsBottom } from "../etc/SocialsBottom";
+import Link from "next/link";
 
 const DownSection = () => {
   return (
-    <section id="down" className="w-full h-fit flex flex-col bg-bottom">
+    <section
+      id="down"
+      className="w-full h-fit flex flex-col bg-bottom relative"
+    >
       <div className="w-full h-fit flex px-4 py-6">
         <TokenomicsPieChart />
       </div>
       <div className="w-full h-[500px] md:h-screen lg:h-[300px] flex items-end md:items-center lg:items-end pb-20 justify-center">
         <SocialsBottom />
+      </div>
+
+      <div className="absolute bottom-4 left-0 w-full bg-background flex items-center justify-center space-x-4 py-2">
+        <Link href={"/"} className="font-bold hover:scale-110">
+          Terms
+        </Link>
+        <Link href={"/"} className="font-bold hover:scale-110">
+          Privacy
+        </Link>
+        <Link href={"/"} className="font-bold hover:scale-110">
+          Cookies
+        </Link>
       </div>
     </section>
   );
